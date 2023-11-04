b054e1a2c3e9b7f6b710354ffa3caea76ff6e227
Mach Solutions <m_fogel4@yahoo.com>
2023-11-03 17:03:05 -0700

    Progress on swap

--- lib/src/frontend/exchange.dart
+++ lib/src/frontend/exchange.dart
@@ -1,124 +1,17 @@
-// import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'package:sui/sui.dart';
 
-// class ExchangePage extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('Exchange Cryptocurrency'),
-//       ),
-//       body: ExchangeForm(),
-//     );
-//   }
-// }
+class ExchangePage extends StatelessWidget {
+  final SuiAccount userAccount;
+  const ExchangePage({super.key, required this.userAccount});
 
-// class ExchangeForm extends StatefulWidget {
-//   @override
-//   _ExchangeFormState createState() => _ExchangeFormState();
-// }
-
-// class _ExchangeFormState extends State<ExchangeForm> {
-//   final _formKey = GlobalKey<FormState>();
-//   String _fromCurrency = 'Bitcoin';
-//   String _toCurrency = 'Ethereum';
-//   double _exchangeRate = 0.0;
-//   double _exchangeAmount = 0.0;
-//   double _estimatedAmount = 0.0;
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Form(
-//       key: _formKey,
-//       child: Column(
-//         crossAxisAlignment: CrossAxisAlignment.start,
-//         children: <Widget>[
-//           DropdownButtonFormField<String>(
-//             value: _fromCurrency,
-//             items: <String>['Bitcoin', 'Ethereum', 'Litecoin', 'Dogecoin']
-//                 .map<DropdownMenuItem<String>>((String value) {
-//               return DropdownMenuItem<String>(
-//                 value: value,
-//                 child: Text(value),
-//               );
-//             }).toList(),
-//             onChanged: (value) {
-//               setState(() {
-//                 _fromCurrency = value;
-//               });
-//               calculateEstimatedAmount();
-//             },
-//             decoration: InputDecoration(
-//               labelText: 'From',
-//             ),
-//           ),
-//           TextFormField(
-//             keyboardType: TextInputType.number,
-//             onChanged: (value) {
-//               setState(() {
-//                 _exchangeAmount = double.tryParse(value) ?? 0.0;
-//               });
-//               calculateEstimatedAmount();
-//             },
-//             decoration: InputDecoration(
-//               labelText: 'Amount',
-//             ),
-//             validator: (value) {
-//               if (value.isEmpty) {
-//                 return 'Please enter an amount';
-//               }
-//               return null;
-//             },
-//           ),
-//           DropdownButtonFormField<String>(
-//             value: _toCurrency,
-//             items: <String>['Bitcoin', 'Ethereum', 'Litecoin', 'Dogecoin']
-//                 .map<DropdownMenuItem<String>>((String value) {
-//               return DropdownMenuItem<String>(
-//                 value: value,
-//                 child: Text(value),
-//               );
-//             }).toList(),
-//             onChanged: (value) {
-//               setState(() {
-//                 _toCurrency = value;
-//               });
-//               calculateEstimatedAmount();
-//             },
-//             decoration: InputDecoration(
-//               labelText: 'To',
-//             ),
-//           ),
-//           TextFormField(
-//             readOnly: true,
-//             initialValue: _estimatedAmount.toStringAsFixed(2),
-//             decoration: InputDecoration(
-//               labelText: 'Estimated Amount',
-//             ),
-//           ),
-//           SizedBox(height: 16.0),
-//           Center(
-//             child: RaisedButton(
-//               onPressed: () {
-//                 if (_formKey.currentState.validate()) {
-//                   submitExchangeRequest();
-//                 }
-//               },
-//               child: Text('Exchange'),
-//             ),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-
-//   void submitExchangeRequest() {
-//     // TODO: Send exchange request to SUI chain API
-//   }
-
-//   void calculateEstimatedAmount() {
-//     // TODO: Calculate estimated amount based on exchange rate
-//     setState(() {
-//       _estimatedAmount = _exchangeAmount * _exchangeRate;
-//     });
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Column(
+        children: [
+        ],
+      ),
+    );
+  }
+}
--- lib/src/services/sui.dart
+++ lib/src/services/sui.dart
@@ -1,6 +1,7 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 import 'package:sui/sui.dart';
 import 'package:flutter_config/flutter_config.dart';
+import 'package:http/http.dart' as http;
 
 final client = SuiClient(Constants.mainnetAPI);
 
@@ -26,4 +27,62 @@ Future<List<CoinBalance>?> fetchCoinData(address) async {
     print(exception);
     return null;
   }
+}
+
+Future<String> swap(userAccount, poolA, poolB, byAmountIn, amount, amountLimit) async {
+  // this is the package ID address of the integrate contract on testnet
+  var packageObjectId = FlutterConfig.get("CETUS_INTEGRATE_PACKAGE_ID");
+  var globalConfigId = FlutterConfig.get("CETUS_GLOBAL_CONFIG_ID");
+  var swapCountUrl = FlutterConfig.get("CETUS_SWAP_COUNT_URL");
+
+  var cetusResponse = await http.get(Uri.parse(swapCountUrl));
+  if (cetusResponse.statusCode != 200) {
+    throw Exception('Failed to load data');
+  }
+  var cetusResponseBody = json.decode(cetusResponse.body);
+
+  // find correct pool - example of pool name: "USDT-USDC"
+  var pools = cetusResponseBody.data.pools;
+  dynamic desiredPool;
+  pools.forEach((pool) {
+    if (pool.symbol == "$poolA-$poolB") {
+      desiredPool = pool;
+    }
+  });
+  if (desiredPool == null) {
+    throw Exception('Could not find pool');
+  }
+
+  // get pool object
+  /* var poolObject = getMoveObject({
+    "id": desiredPool.swap_account,
+    "options": {
+      "showContent": true,
+      "showType": true
+    }
+  }); */
+
+  // get coin vector
+  final tx = TransactionBlock();
+  var coinsAVec = tx.makeMoveVec(objects: {});
+
+  tx.setGasBudget(BigInt.from(20000000));
+
+  // TODO: support swapping from b to a, handle logic of byAmountIn too  
+  tx.moveCall("$packageObjectId::pool_script::swap_a2b", arguments: [
+    tx.pure(globalConfigId), // config: &GlobalConfig,
+    tx.pure(desiredPool.swap_account), // pool: &mut Pool<CoinTypeA, CoinTypeB>,
+    coinsAVec, // coins_a: vector<Coin<CoinTypeA>>,
+    tx.pure(byAmountIn), // by_amount_in: bool,
+    tx.pure(amount), // amount: u64,
+    tx.pure(amountLimit), // amount_limit: u64,
+    tx.pure(4295048016), // sqrt_price_limit: u128,
+    tx.pure("0x6") // clock: &Clock
+    // do we need to include ctx: &mut TxContext here?
+    // it is not a variable on https://suiexplorer.com/object/0x8627c5cdcd8b63bc3daa09a6ab7ed81a829a90cafce6003ae13372d611fbb1a9?module=pool_script&network=testnet
+    // but it is a variable on https://github.com/CetusProtocol/cetus-clmm-interface/blob/577fd059bdf9c7d7c1787e1ff233cda5a8b63c44/README.md?plain=1#L904
+  ]);
+
+  final result = await client.signAndExecuteTransactionBlock(userAccount, tx);
+  return result.digest;
 }
