3a5026f839ba18d62cfda9cbed8513be9f83f851
Sena Hazir <senahazir@berkeley.edu>
2023-11-05 01:15:37 -0700

    homepage fixes

--- lib/src/frontend/home.dart
+++ lib/src/frontend/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:sui/sui.dart';
 import 'package:suiinvest/src/frontend/common/helpers/string.dart';
+
 class HomePage extends StatelessWidget {
   // props
   final SuiAccount userAccount;
@@ -39,6 +40,7 @@ class HomePage extends StatelessWidget {
   Widget buildPortfolio(BuildContext context) {
     return Center(
       child: Container(
+        height: 100,
         width: MediaQuery.of(context).size.width,
         padding: EdgeInsets.all(16.0),
         decoration: BoxDecoration(
@@ -151,20 +153,35 @@ class EcosystemItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 8.0),
-      child: Row(
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10.0),
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
+      decoration: BoxDecoration(
+        color: Color(0xFF1A1A1A),
+        borderRadius: BorderRadius.circular(12.0),
+        border: Border.all(
+            color: Colors
+                .grey[800]!), // Adjust the border color to match your theme
+      ),
+      child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text(label, style: TextStyle(color: Colors.white, fontSize: 16.0)),
+          Text(label,
+              style: TextStyle(
+                  color: Color.fromRGBO(187, 205, 255, 100), fontSize: 16.0)),
           Row(
             children: [
               Text(value,
                   style: TextStyle(color: Colors.white, fontSize: 16.0)),
-              if (change != null) SizedBox(width: 8.0),
-              if (change != null)
+              if (change != null) ...[
+                SizedBox(width: 50.0),
                 Text(change!,
-                    style: TextStyle(color: Colors.red, fontSize: 16.0)),
+                    style: TextStyle(
+                        color:
+                            change!.contains('-') ? Colors.red : Colors.green,
+                        fontSize: 16.0)),
+              ],
             ],
           ),
         ],
@@ -174,32 +191,23 @@ class EcosystemItem extends StatelessWidget {
 }
 
 Widget buildHealth(BuildContext context) {
-  return Container(
-    padding: EdgeInsets.all(16.0),
-    decoration: BoxDecoration(
-      borderRadius: BorderRadius.circular(12.0),
-      color: Color(0xFF1A1A1A),
-    ),
-    child: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text('Ecosystem Health',
-            style: TextStyle(
-                color: Colors.white,
-                fontSize: 18.0,
-                fontWeight: FontWeight.bold)),
-        SizedBox(height: 16.0),
-        EcosystemItem(label: 'Market Cap', value: '\$356,104,841'),
-        EcosystemItem(label: 'Price', value: '\$0.418026', change: '-12.4%'),
-        EcosystemItem(label: '24hr Trading Vol', value: '\$29,026,989'),
-        EcosystemItem(
-            label: 'Fully Diluted Valuation', value: '\$4,138,862,797'),
-        EcosystemItem(label: 'Circulating Supply', value: '\$860,392,9597'),
-      ],
-    ),
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      Text('Ecosystem Health',
+          style: TextStyle(
+              color: Colors.white,
+              fontSize: 18.0,
+              fontWeight: FontWeight.bold)),
+      SizedBox(height: 16.0),
+      EcosystemItem(label: 'Market Cap', value: '\$356,104,841'),
+      EcosystemItem(label: 'Price', value: '\$0.418026', change: '-12.4%'),
+      EcosystemItem(label: '24hr Trading Vol', value: '\$29,026,989'),
+      EcosystemItem(label: 'Fully Diluted Valuation', value: '\$4,138,862,797'),
+      EcosystemItem(label: 'Circulating Supply', value: '\$860,392,9597'),
+    ],
   );
 }
-
 
 // example of how to use user fetched coins in front end
 Widget listCoins(BuildContext context, List<CoinBalance> userBalances) {
@@ -211,11 +219,10 @@ Widget listCoins(BuildContext context, List<CoinBalance> userBalances) {
     ),
     child: Column(
       crossAxisAlignment: CrossAxisAlignment.start,
-      children: 
-        userBalances.map((coin) {
-          return Text(coin.totalBalance.toString(),
-              style: TextStyle(fontSize: 16));
-        }).toList(),
+      children: userBalances.map((coin) {
+        return Text(coin.totalBalance.toString(),
+            style: TextStyle(fontSize: 16));
+      }).toList(),
     ),
   );
 }
