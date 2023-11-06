a9cedf0ec0bc41d23ef2390c819a0252eda022c8
Lawson Graham <lawson.graham8@gmail.com>
2023-11-06 15:01:50 -0800

    f

--- lib/src/common/constants/addresses.dart
+++ lib/src/common/constants/addresses.dart
@@ -8,3 +8,4 @@ const SUI_CLOCK = "0x6";
 // const CETUS_INTEGRATE_PACKAGE_ID="0x996c4d9480708fb8b92aa7acf819fb0497b5ec8e65ba06601cae2fb6db3312c3";
 // const CETUS_GLOBAL_CONFIG_ID = "0xdaa46292632c3c4d8f31f23ea0f9b36a28ff3677e9684980e4438403a67a3d8f";
 // const CETUS_SWAP_COUNT_URL = "https://api-sui.cetus.zone/v2/sui/swap/count";
+// const SUI_CLOCK = "0x6";
