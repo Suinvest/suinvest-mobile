915372d07d05d727d8283a75e58cbfa31794f66e
Lawson Graham <lawson.graham8@gmail.com>
2023-11-07 22:25:58 -0800

    added coingecko market history fetcher

--- lib/src/services/coingecko.dart
+++ lib/src/services/coingecko.dart
@@ -1,23 +1,34 @@
 import 'package:coingecko_api/coingecko_api.dart';
 import 'package:coingecko_api/data/market.dart';
+import 'package:coingecko_api/data/market_chart_data.dart';
 
 Future<List<Market>?> fetchCoinPrices(
-      List<String> coinIds, String currency) async {
-    final api = CoinGeckoApi();
-    print('fetching coinIds in fetchCoinPrices()');
-    print(coinIds);
-    final result = await api.coins.listCoinMarkets(
-      vsCurrency: currency,
-      coinIds: coinIds,
-    );
-    print('response of coin data in fetchCoinPrices()');
-    print(result);
-    if (!result.isError) {
-      return result.data;
-    } else {
-      print('getCoinOHLC() method returned error:');
-      print('${result.errorCode}: ${result.errorMessage}');
-      print('Test method failed.');
-    }
-    return null;
+    List<String> coinIds, String currency) async {
+  final api = CoinGeckoApi();
+  final result = await api.coins.listCoinMarkets(
+    vsCurrency: currency,
+    coinIds: coinIds,
+  );
+  if (!result.isError) {
+    return result.data;
+  } else {
+    print('${result.errorCode}: ${result.errorMessage}');
   }
+  return null;
+}
+
+Future<List<MarketChartData>?> fetchCoinHistory(
+    String coinId, String currency, int priorDays) async {
+  final api = CoinGeckoApi();
+  final result = await api.coins.getCoinMarketChart(
+    id: coinId,
+    vsCurrency: currency,
+    days: priorDays,
+  );
+  if (!result.isError) {
+    return result.data;
+  } else {
+    print('${result.errorCode}: ${result.errorMessage}');
+  }
+  return null;
+}
