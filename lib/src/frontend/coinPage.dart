ee5d8b5f143e5c63cf1ab580914f71c3745397ab
Lawson Graham <lawson.graham8@gmail.com>
2023-11-09 12:27:05 -0800

    added defillama service

--- lib/src/frontend/coinPage.dart
+++ lib/src/frontend/coinPage.dart
@@ -87,7 +87,6 @@ class CryptoListItem extends StatelessWidget {
 }
 
 Future<List<CryptoListItem>> fetchCoins() async {
-  final res = await fetchSUIData();
   final response = await http.get(Uri.parse(
       'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false'));
   if (response.statusCode == 200) {
--- lib/src/services/defillama.dart
+++ lib/src/services/defillama.dart
@@ -3,38 +3,71 @@ import 'dart:convert';
 
 const defillamaBaseUrl = 'https://api.llama.fi';
 
+class TVLTimestamp {
+  final int timestamp;
+  final double tvl;
 
-class CryptoListItem {
-  final int dailyChange;
+  const TVLTimestamp({
+    required this.timestamp,
+    required this.tvl,
+  });
+}
+
+class DEXVolume {
+  final int defillamaId;
   final String name;
-  final String symbol;
-  final String price;
-  final String change;
-  final String iconUrl;
-
-  const CryptoListItem({
-    required this.rank,
+  final String displayName;
+  final String logo;
+  final double change_1m;
+  final double change_1d;
+  final double change_7d;
+  final double total24h;
+  final double dailyVolume;
+  final double totalVolume7d;
+  final double totalVolume30d;
+  final double totalAllTime;
+  
+  const DEXVolume({
+    required this.defillamaId,
     required this.name,
-    required this.symbol,
-    required this.price,
-    required this.change,
-    required this.iconUrl,
+    required this.displayName,
+    required this.logo,
+    required this.change_1m,
+    required this.change_1d,
+    required this.change_7d,
+    required this.total24h,
+    required this.dailyVolume,
+    required this.totalVolume7d,
+    required this.totalVolume30d,
+    required this.totalAllTime,
+  });
+}
+
+class SUIData {
+  final List<TVLTimestamp> tvlHistory;
+  final List<DEXVolume> dexVolumes;
+  // final dynamic protocolsFees;
+
+  const SUIData({
+    required this.tvlHistory,
+    required this.dexVolumes,
+    // required this.protocolsFees,
   });
 }
 
 
-Future<List<SUIData>?> fetchSUIData() async {
-    print ("fetching data");
-    final response = await http.get(Uri.parse('${defillamaBaseUrl}/overview/dexs/sui'));
-  if (response.statusCode == 200) {
-    final res = json.decode(response.body);
-    final protocols = res['protocols'];
-    print (protocols);
+Future<SUIData?> fetchSUIData() async {
+  final dexVolsResponse = await http.get(Uri.parse('${defillamaBaseUrl}/overview/dexs/sui'));
+  // final protocolsFeesResponse = await http.get(Uri.parse('${defillamaBaseUrl}/overview/fees/sui'));
 
-    protocols.forEach((protocol) {
-      print (protocol['name']);
-    });
-    return ["re"];
+  final tvlHistoryResponse = await http.get(Uri.parse('${defillamaBaseUrl}/v2/historicalChainTvl/sui'));
+  if (tvlHistoryResponse.statusCode == 200 && dexVolsResponse.statusCode == 200) {
+    final dexVolsJson = json.decode(dexVolsResponse.body);
+    // final protocolsFeesJson = json.decode(protocolsFeesResponse.body);
+    final tvlHistory = json.decode(tvlHistoryResponse.body);
+    final dexVols = dexVolsJson['protocols'];
+    
+    return SUIData(tvlHistory: tvlHistory, dexVolumes: dexVols);
   } else {
     throw Exception('Failed to load coins');
   }
